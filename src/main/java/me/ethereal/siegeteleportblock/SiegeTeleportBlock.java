42315c9f246a7972d97d18e835da8cec5b95e569
MKieburtz <michaelkieburtz@gmail.com>
2021-01-27 14:06:47 -0800

    First Commit

---
+++ src/main/java/me/ethereal/siegeteleportblock/SiegeTeleportBlock.java
@@ -0,0 +1,18 @@
+package me.ethereal.siegeteleportblock;
+
+import com.palmergames.bukkit.towny.TownyAPI;
+import org.bukkit.plugin.java.JavaPlugin;
+import com.gmail.goosius.siegewar.*;
+
+public final class SiegeTeleportBlock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
